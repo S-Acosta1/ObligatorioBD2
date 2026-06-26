1475e5d3ed26fd8a5c0a0000dcb9d110d69a8f1d
Guzman Zugnoni <guzman.zugnoni@correo.ucu.edu.uy>
2026-06-26 17:02:32 -0300

    cambiar contra de usuario de prueba

--- sql/03_datos_prueba.sql
+++ sql/03_datos_prueba.sql
@@ -123,7 +123,7 @@ INSERT INTO `EventoHabilitaSector` (`id_evento`, `id_sector`, `nombre_estadio`) 
 --
 
 INSERT INTO `Usuario` VALUES
-	('test@example.com','Test Usuario','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92',1,'12345678','URU',1,'URU');
+	('test@example.com','Usuario Test','b460b1982188f11d175f60ed670027e1afdd16558919fe47023ecd38329e0b7f',1,'12345678','URU',1,'URU'); -- Contra hola123
 
 --
 -- Dumping data for table `UsuarioGeneral`
